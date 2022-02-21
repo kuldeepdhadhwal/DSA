70bc2abb95ec260322f3282c2c83cf0afdb0ca95
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2022-02-21 23:19:42 +0530

    Update MoveZeros.py
    updated the code

--- MoveZeros.py
+++ MoveZeros.py
@@ -1,17 +1,20 @@
 class Solution:
-    # https://leetcode.com/problems/move-zeroes/
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        out = []
-        count = 0
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                count +=1
-        add = [0]*count
-        for i in range(len(nums)):
-            if nums[i] !=0:
-                out.append(nums[i])
+        i = 0
+        zeroCounter = 0
         
-        print(out+add)
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i] = nums[j]
+                i +=1
+            else:
+                zeroCounter +=1
+        while i < len(nums):
+            nums[i] = 0
+            i +=1
+        
+        print(nums)
+                
