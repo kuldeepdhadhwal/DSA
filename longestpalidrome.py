94014ebdc6ba7ee80e65ab0f1d71cb93b3d9f78d
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2022-02-27 22:05:24 +0530

    Create longestpalidrome.py

---
+++ longestpalidrome.py
@@ -0,0 +1,24 @@
+class Solution:
+    # https://leetcode.com/problems/longest-palindromic-substring/submissions/
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        result = 0
+        for i in range(len(s)):
+            l,r = i,i
+            while l >=0 and r < len(s) and s[l] == s[r]:
+                if (r-l + 1) > result:
+                    res = s[l:r+1]
+                    result = r-l+1
+                l -=1
+                r +=1
+                
+#           even case
+            l,r = i,i+1
+            while l >=0 and r < len(s) and s[l] == s[r]:
+                if (r-l + 1) > result:
+                    res = s[l:r+1]
+                    result = r-l+1
+                l -=1
+                r +=1
+            
+        return res
