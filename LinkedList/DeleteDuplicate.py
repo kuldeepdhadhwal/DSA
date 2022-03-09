da47dd547cc8c35eb5eb411f7c159cec61e2e48c
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2022-03-09 22:08:20 +0530

    Create DeleteDuplicate.py

---
+++ LinkedList/DeleteDuplicate.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0);  # construct a dummy node
+        dummy.next = head 
+
+        pre = dummy           # set up pre and cur pointers
+        cur = head
+        while cur:
+            if cur.next and cur.val == cur.next.val:
+                # loop until cur point to the last duplicates
+                while cur and cur.next and cur.val == cur.next.val:
+                    cur = cur.next
+                pre.next = cur.next  # propose the next for pre
+                                     # this will be verified by next line
+            else:
+                pre = pre.next 
+            cur = cur.next
+        return dummy.next
+        
