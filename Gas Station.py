ccfd6fe5611287285ab6dc59067043cc7d154b8c
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2022-03-11 22:39:29 +0530

    Update Gas Station.py

--- Gas Station.py
+++ Gas Station.py
@@ -9,13 +9,13 @@ class Solution:
 	# @param B : tuple of integers
 	# @return an integer
 	def canCompleteCircuit(self, petrol, dist):
-        n = len(petrol)
-        start,petrolsum,distsum = 0,0,0
-        for i in range(n):
-            petrolsum+=(petrol[i]-dist[i])
-            distsum+=(petrol[i]-dist[i])
-            if distsum<0:
-                start=i+1
-                distsum = 0
-        if petrolsum>=0:return((start)%n)
-        else:return(-1)
+		n = len(petrol)
+		start,petrolsum,distsum = 0,0,0
+		for i in range(n):
+		    petrolsum+=(petrol[i]-dist[i])
+		    distsum+=(petrol[i]-dist[i])
+		    if distsum<0:
+			start=i+1
+			distsum = 0
+		if petrolsum>=0:return((start)%n)
+		else:return(-1)
