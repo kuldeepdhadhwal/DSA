d9721593568fc8e5f4b1743179359e1547cab965
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2022-03-12 23:04:08 +0530

    Create Robber-house.py

---
+++ DP/Robber-house.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev = curr = 0
+        for num in nums:
+            temp = prev # This represents the nums[i-2]th value
+            prev = curr # This represents the nums[i-1]th value
+            curr = max(num + temp, prev) # Here we just plug into the formula
+        return curr
+            
