d30c2f208d143e9b7a7c637c736bff2a80e9af85
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2022-03-25 00:14:38 +0530

    Update brokenCalculator.py

--- brokenCalculator.py
+++ brokenCalculator.py
@@ -8,6 +8,18 @@ class Solution:
             return 1 + self.brokenCalc(startValue, target // 2)
         else:
             return 1 + self.brokenCalc(startValue, target + 1)
+        
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        steps = 0
+        while target > startValue:
+            steps += 1
+            # Base Greedy Condition
+            if target % 2 == 0:
+                target //= 2
+            else:
+                target += 1
+        # Once target <= startValue, we only do subtract 1 to fit the target
+        return steps + startValue - target
           
          
         
