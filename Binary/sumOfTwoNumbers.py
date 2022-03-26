e33ff81ab467dcae7c6dd3d790a0e945caff0e03
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2022-03-26 17:43:17 +0530

    Create sumOfTwoNumbers.py

---
+++ Binary/sumOfTwoNumbers.py
@@ -0,0 +1,13 @@
+class Solution:
+#   https://leetcode.com/problems/sum-of-two-integers/
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0b11111111111111111111111111111111       
+        MAX =  0b01111111111111111111111111111111
+        
+        if b == 0:
+            return a if a <= MAX else ~(a ^ mask)
+        
+        return self.getSum(
+            (a ^ b) & mask,
+            ((a & b) << 1) & mask
+        )
