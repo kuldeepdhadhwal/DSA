063ff9b3e68775db0e3ed6cc61565bf030d22a4c
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2022-03-28 22:23:08 +0530

    Create search_in_sorted_array.py

---
+++ leetcode/dailyChallenge/search_in_sorted_array.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        for i in nums:
+            if i == target:
+                return True
+        return False
+
