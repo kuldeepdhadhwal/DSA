e6f47b1304aec8c27d6fc2655bd089f009f1f1a4
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2022-04-05 23:11:54 +0530

    Update containerwithMostWater.py
    updated the code with algorithm

--- leetcode/dailyChallenge/containerwithMostWater.py
+++ leetcode/dailyChallenge/containerwithMostWater.py
@@ -1,4 +1,14 @@
 class Solution:
+#     https://leetcode.com/problems/container-with-most-water/
+
+'''
+Steps for Algorithm
+1. First take i,j as a pointer representing the initial and end,
+2. Then using a while loop runs till i < j
+3. take max of water having water, min of i, j height by multiplying it with j-i
+4. check the height and update the i if height[i] < height[j] else j-=1
+
+'''
     def maxArea(self, height: List[int]) -> int:
         i,j = 0, len(height)-1
         water = 0
