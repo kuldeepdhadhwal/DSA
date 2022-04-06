d1c839dccd66a1ebe8736afd8eab0141eb143f24
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2022-04-06 22:44:32 +0530

    Create 3sumMuli.py

---
+++ LinkedList/3sumMuli.py
@@ -0,0 +1,12 @@
+class Solution:
+#   https://leetcode.com/problems/3sum-with-multiplicity/
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
+        ans = 0
+        n = len(arr)
+        level2 = collections.defaultdict(int)
+        for i in range(2, n):
+            for j in range(i-1):
+                level2[arr[j] + arr[i-1]] += 1
+            ans = ans + level2[target - arr[i]]
+            ans = ans % (10**9 + 7)
+        return ans
