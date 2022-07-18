f8b77a57f600358a1ece595b42c252716a791418
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2022-07-18 15:46:42 +0530

    Create binary_tree_level_order.py

---
+++ trees/binary_tree_level_order.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/binary-tree-level-order-traversal/submissions/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root: return []
+        queue, result = deque([root]), []
+        while queue:
+            level = []
+            for i in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+        return result
+                    
